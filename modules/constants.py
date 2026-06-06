0c8abfaa59f16d3bd546cbbd7265856c957549cd
pritishmedhi2018-design <pritishmedhi2018@gmail.com>
2026-06-06 14:21:11 +0530

    Create constants.py

---
+++ modules/constants.py
@@ -0,0 +1,15 @@
+PHOTO_SIZES = {
+    "Passport": (35,45),
+    "ID Card": (25,35),
+    "1×1 inch": (25,25),
+    "2×2 inch": (51,51),
+    "Visa": (35,45)
+}
+
+BG_COLORS = {
+    "White":"white",
+    "Blue":"#87CEEB",
+    "Red":"#FF4D4D",
+    "Light Grey":"#D3D3D3",
+    "Green":"#90EE90"
+}
